8667d99ccdabd2580f2e4f326e7c4efc1dd4dc78
y.roullard <y.roullard@overkiz.com>
2016-05-31 16:11:51 +0200

    create proxy to backend server

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var express = require('express');
+var proxy = require('proxy-middleware');
+var url = require('url');
+
+//--------your proxy----------------------
+var app = express();
+//proxy the request for static assets
+app.use('/api', proxy(url.parse('API_END_POINT')));
+
+
+
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -13,3 +24,4 @@ new WebpackDevServer(webpack(config), {
     }
     console.log('Listening at localhost:5000');
   });
+app.listen(5001);
